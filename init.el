2eed1abc4dc55abd3b747edbac09066f5831fa86
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2013-04-11 09:04:56 -0400

    colors and more

---
+++ init.el
@@ -0,0 +1,50 @@
+(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
+
+;; COLOR-THEME
+;;
+(add-to-list 'load-path "~/.emacs.d/color-theme")
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-hober)))
+(load-file "~/.emacs.d/cyberpunk.el")
+(color-theme-cyberpunk)
+(set-cursor-color "yellow")
+(require 'live-fontify-hex)
+
+;; MARKDOWN
+;;
+(autoload 'markdown-mode "markdown-mode/markdown-mode"
+   "Major mode for editing Markdown files" t)
+
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; GLSL 
+;;
+(autoload 'glsl-mode "glsl-mode/glsl-mode" nil t)
+  (add-to-list 'auto-mode-alist '("\\.glsl\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
+  (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode))
+
+;; AUTOCOMPLETE
+;;
+(add-to-list 'load-path "~/.emacs.d/auto-complete")
+(require 'auto-complete-config)
+(ac-config-default) 
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
